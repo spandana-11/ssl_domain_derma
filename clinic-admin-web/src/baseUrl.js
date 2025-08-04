1bc07e6e357604cc4ddae5ae6a276090432384f2
spandana <spandanaguntupalli@gmail.com>
2025-08-04 11:16:16 +0530

    changes on 4/08/2025

--- clinic-admin-web/src/baseUrl.js
+++ clinic-admin-web/src/baseUrl.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'http://alb-dev-sc-197990416.ap-south-1.elb.amazonaws.com/api'
-export let wifiUrl = '13.201.155.168:9090'
+export let wifiUrl = '13.203.6.65:9090'
 // export let wifiUrl = '192.168.1.5'
 
 export const BASE_URL = `http://${wifiUrl}/clinic-admin`
